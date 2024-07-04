23425b9707f4e4b0b128c889f71c1c6aaf374897
sanjayv23 <sanjayvelu2311@gmail.com>
2024-07-04 15:08:52 +0530

    feat: delete specify post

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 import express from "express"
 import bodyparser from "body-parser";
+import { v4 as uuidv4 } from 'uuid';
 const app=express()
 const port=3000
-let array=[];
+let posts=[];
 let i=0;
 let j=0;
 
@@ -12,22 +13,27 @@ app.use(bodyparser.urlencoded({extended: true}));
 app.get("/",(req,res)=>{
     
     res.render("index.ejs",{
-        prevmessage:array
+        posts:posts
     })
    
 })
 
-app.post("/", (req, res) => {
-    array[i++]=req.body["content"]
-    console.log(array)
-    
-    res.render("content.ejs",{
-        contents:array
-    })
-    res.redirect('/');
+app.post("/add-post", (req, res) => {
+    let newpost={id:uuidv4(),content:req.body["post"]}
+    posts.push(newpost)
+    console.log(posts)
+    res.redirect("/")
     
     
 });
+app.post("/delete-post",(req,res)=>{
+    const postId=req.body["postId"]
+    posts = posts.filter(post => post["id"] !== postId);
+    res.redirect("/")
+})
+app.post("/update-post",(req,res)=>{
+    const postId=req.body["postId"]
+})
   
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
